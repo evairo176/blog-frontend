a496b003519e7c92d3ed1837e8566e8676638435
Dicki Prasetya <dickiprasetya176@gmail.com>
2023-01-11 10:38:55 +0700

    feat: update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import "./App.css";
-
+// import "./index.css";
 function App() {
   return (
     <div>
-      <h1>Blog App</h1>
+      <div className="text-3xl font-bold underline text-yellow-400">
+        Blog App one
+      </div>
     </div>
   );
 }
