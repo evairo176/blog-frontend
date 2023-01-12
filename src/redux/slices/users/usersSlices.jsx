654c4f0a13678c5e5cfaf9f8bdd185097c20b1eb
Dicki Prasetya <dickiprasetya176@gmail.com>
2023-01-12 17:11:21 +0700

    feat: update

--- src/redux/slices/users/usersSlices.jsx
+++ src/redux/slices/users/usersSlices.jsx
@@ -7,7 +7,7 @@ import axios from "axios";
 
 export const registerUserAction = createAsyncThunk(
   "users/register",
-  async (user, { rejectWithValue, getState, dispatch }) => {
+  async (value, { rejectWithValue, getState, dispatch }) => {
     try {
       // http call
       const config = {
@@ -15,8 +15,17 @@ export const registerUserAction = createAsyncThunk(
           "Content-Type": "application/json",
         },
       };
-
-      const res = await axios.request;
-    } catch (error) {}
+      const { data } = await axios.post(
+        `${process.env.REACT_APP_API_URL}/users/register`,
+        value,
+        config
+      );
+      return data;
+    } catch (error) {
+      if (!error?.response) {
+        throw error;
+      }
+      return rejectWithValue(error?.response?.data);
+    }
   }
 );
