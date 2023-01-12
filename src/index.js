8a44a0aa6c7f210757107d3947b308e900aba231
Dicki Prasetya <dickiprasetya176@gmail.com>
2023-01-12 16:48:30 +0700

    feat: update

--- src/index.js
+++ src/index.js
@@ -5,11 +5,17 @@ import ReactDOM from "react-dom/client";
 // import "./styles/tailwind.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/css/custom.css";
+import { Provider } from "react-redux";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import store from "./redux/store/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
