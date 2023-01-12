309e13bb91f3aa5cd7fa97c11d2e883a1caefecc
Dicki Prasetya <dickiprasetya176@gmail.com>
2023-01-12 13:31:29 +0700

    feat: update

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 // import "./styles/tailwind.css";
 // import "./index.css";
 // import "./styles/tailwind.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
