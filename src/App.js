4c9254571acdb281d95fd90a576d7095beacba5e
Dicki Prasetya <dickiprasetya176@gmail.com>
2023-01-12 14:25:51 +0700

    feat: update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { Fragment } from "react";
+import AppRoutes from "./routes/AppRoutes";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <Fragment>
-      <div className="p-6 items-center justify-center">
-        <h1 className="text-blue-400 font-extrabold">Hello World!</h1>
-        <p className="tracking-widest">This is my first React App.</p>
-      </div>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
     </Fragment>
   );
 }
---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function AppRoutes() {
+  return <div>AppRoutes</div>;
+}
+
+export default AppRoutes;
