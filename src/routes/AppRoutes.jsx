68d7e174db905be93819faab6a9b417c81d99c65
Dicki Prasetya <dickiprasetya176@gmail.com>
2023-01-14 15:30:46 +0700

    feat: make navbar

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,5 +1,6 @@
 import React, { Fragment } from "react";
 import { Route, Routes } from "react-router-dom";
+import Navbar from "../components/cummon/Navbar";
 import HomePage from "../pages/home/HomePage";
 import LoginPage from "../pages/user/auth/LoginPage";
 import RegisterPage from "../pages/user/auth/RegisterPage";
@@ -7,6 +8,7 @@ import RegisterPage from "../pages/user/auth/RegisterPage";
 function AppRoutes() {
   return (
     <Fragment>
+      <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/register" element={<RegisterPage />} />
