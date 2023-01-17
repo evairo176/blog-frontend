6c79c9cf0e3a1e8adc1c4a8c5433c1966110cb58
Dicki Prasetya <dickiprasetya176@gmail.com>
2023-01-17 10:01:02 +0700

    feat: update

--- src/utils/dateFormatter.jsx
+++ src/utils/dateFormatter.jsx
@@ -4,6 +4,7 @@ import Moment from "react-moment";
 function DateFormatter(props) {
   return (
     <>
+      {/* // moment  */}
       <Moment format="D MMM YYYY" withTitle>
         {props.date}
       </Moment>
