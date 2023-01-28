cb3d06674a938d27a81ebda08061f189168a0f11
Dicki Prasetya <dickiprasetya176@gmail.com>
2023-01-28 21:18:37 +0700

    feat: update

--- src/utils/ListPostComponent.jsx
+++ src/utils/ListPostComponent.jsx
@@ -2,6 +2,9 @@ import moment from "moment";
 import React from "react";
 import LoadingComponent from "./LoadingComponent";
 import LongText from "./LongText";
+import ThumbUpAltIcon from "@mui/icons-material/ThumbUpAlt";
+import ThumbDownAltIcon from "@mui/icons-material/ThumbDownAlt";
+import VisibilityIcon from "@mui/icons-material/Visibility";
 
 function ListPostComponent({ data, loading, page, limit, total }) {
   return (
@@ -37,6 +40,17 @@ function ListPostComponent({ data, loading, page, limit, total }) {
                 <div className="col-xl-4 col-md-4 col-lg-4 col-sm-12 col-xs-12">
                   <div className="image-post">
                     <img loading="lazy" src={`${row.image}`} alt="post" />
+                  </div>
+                  <div className="action-dwad">
+                    <div className="action">
+                      <ThumbUpAltIcon />
+                    </div>
+                    <div className="action">
+                      <ThumbDownAltIcon />
+                    </div>
+                    <div className="action">
+                      <VisibilityIcon />
+                    </div>
                   </div>
                 </div>
               </div>
