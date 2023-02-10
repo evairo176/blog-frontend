5645c7f84d4af5ba8434039cfc912a69f83161ee
Dicki Prasetya <dickiprasetya176@gmail.com>
2023-02-10 19:10:38 +0700

    feat: update

--- src/components/posts/PostDetail.jsx
+++ src/components/posts/PostDetail.jsx
@@ -5,13 +5,14 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchPostById } from "../../redux/slices/posts/postSlices";
 import Moment from "react-moment";
+import { Skeleton } from "@mui/material";
 
 function PostDetail() {
   const params = useParams();
   const dispatch = useDispatch();
 
   const storeData = useSelector((store) => store?.posts);
-  const { postDetail } = storeData;
+  const { postDetail, loading } = storeData;
 
   useEffect(() => {
     dispatch(fetchPostById(params?.id));
@@ -20,44 +21,102 @@ function PostDetail() {
   return (
     <>
       <Navbar />
-      <div className="container-content-blog">
-        <section className="section-posts-detail">
-          <div className="profile-user-post">
-            <div className="date">
-              <Moment format="D MMMM YYYY" withTitle>
-                {postDetail?.createdAt}
-              </Moment>
-              <span className="titik-bulet"></span>{" "}
-              <span className="kdwaidk">{postDetail?.category}</span>
+      {loading ? (
+        <div className="container-content-blog">
+          <section className="section-posts-detail">
+            <div className="profile-user-post">
+              <div className="date">
+                <Skeleton width={100} height={22} />
+                <Skeleton width={100} height={22} />
+              </div>
+              <div className="post-title">
+                <Skeleton width={100} height={42} />{" "}
+              </div>
+              <div className="name-and-foto">
+                <Skeleton variant="circular" width={40} height={40} />
+                <span>
+                  <Skeleton width={100} height={22} />
+                </span>
+              </div>
             </div>
-            <div className="post-title">{postDetail?.title}</div>
-            <div className="name-and-foto">
-              <img
-                width={45}
-                src={`${postDetail?.user?.profilePhoto}`}
-                className="rounded-circle"
-                alt={postDetail?.user?.firstName}
-              />
+            <div className="image-posts-details">
+              <Skeleton width="100%" height={550} />
+            </div>
+            <div className="post-detail-description">
+              <Skeleton width="100%" height={22} />
+              <Skeleton width="100%" height={22} />
+              <Skeleton width="100%" height={22} />
+              <Skeleton width="100%" height={22} />
+              <Skeleton width="100%" height={22} />
+              <Skeleton width="100%" height={22} />
+              <Skeleton width="100%" height={22} />
+              <Skeleton width="100%" height={22} />
+              <Skeleton width="100%" height={22} />
+              <Skeleton width="100%" height={22} />
+              <Skeleton width="100%" height={22} />
+              <Skeleton width="100%" height={22} />
+              <Skeleton width="100%" height={22} />
+              <Skeleton width="100%" height={22} />
+              <Skeleton width="100%" height={22} />
+              <Skeleton width="100%" height={22} />
+              <Skeleton width="100%" height={22} />
+              <Skeleton width="100%" height={22} />
+              <Skeleton width="100%" height={22} />
+              <Skeleton width="100%" height={22} />
+              <Skeleton width="100%" height={22} />
+              <Skeleton width="100%" height={22} />
+              <Skeleton width="100%" height={22} />
+              <Skeleton width="100%" height={22} />
+              <Skeleton width="100%" height={22} />
+              <Skeleton width="100%" height={22} />
+              <Skeleton width="100%" height={22} />
+              <Skeleton width="100%" height={22} />
+              <Skeleton width="100%" height={22} />
+              <Skeleton width="100%" height={22} />
+              <Skeleton width="100%" height={22} />
+            </div>
+          </section>
+        </div>
+      ) : (
+        <div className="container-content-blog">
+          <section className="section-posts-detail">
+            <div className="profile-user-post">
+              <div className="date">
+                <Moment format="D MMMM YYYY" withTitle>
+                  {postDetail?.createdAt}
+                </Moment>
+                <span className="titik-bulet"></span>{" "}
+                <span className="kdwaidk">{postDetail?.category}</span>
+              </div>
+              <div className="post-title">{postDetail?.title}</div>
+              <div className="name-and-foto">
+                <img
+                  width={45}
+                  src={`${postDetail?.user?.profilePhoto}`}
+                  className="rounded-circle"
+                  alt={postDetail?.user?.firstName}
+                />
 
-              <span>
-                {postDetail?.user?.firstName} {postDetail?.user?.lastName}
-              </span>
+                <span>
+                  {postDetail?.user?.firstName} {postDetail?.user?.lastName}
+                </span>
+              </div>
             </div>
-          </div>
-          <div className="image-posts-details">
-            <img
-              src={`${postDetail?.image}`}
-              alt=""
-              className="img-post-detail"
-            />
-          </div>
-          <div className="post-detail-description">
-            <div
-              dangerouslySetInnerHTML={{ __html: postDetail?.description }}
-            ></div>
-          </div>
-        </section>
-      </div>
+            <div className="image-posts-details">
+              <img
+                src={`${postDetail?.image}`}
+                alt=""
+                className="img-post-detail"
+              />
+            </div>
+            <div className="post-detail-description">
+              <div
+                dangerouslySetInnerHTML={{ __html: postDetail?.description }}
+              ></div>
+            </div>
+          </section>
+        </div>
+      )}
     </>
   );
 }
